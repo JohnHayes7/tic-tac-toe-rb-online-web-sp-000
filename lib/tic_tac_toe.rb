78ec02d3bfc595817df2e848c5b25fbc238457ec
John Hayes <johnnyhayes7@gmail.com>
2019-01-21 23:03:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,8 +42,8 @@ end
 
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0, 8)
-      return true
+  if !position_taken?(board, index) && index.between?(0, 8)
+    return true
     else
       return false
   end
